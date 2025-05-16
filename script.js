151686d706865f64705aebea9c2df2638ef2450f
Ozge Ozer <104353749+ozgeozerr@users.noreply.github.com>
2025-05-16 22:53:25 +0300

    V1

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementsByClassName("lottie-container")[0],
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/complink.json",
+});
